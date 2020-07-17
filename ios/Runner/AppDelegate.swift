757cbba3e87902cb6d3dc2a1d849a9fc9d722b71
Valerii Babanin <ilo.xsv@gmail.com>
2020-07-17 10:34:16 +0300

    ios fixes

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -6,9 +6,10 @@ import YandexMapKit
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     YMKMapKit.setApiKey("721bea2e-5bfb-48d2-9a3f-f5e6311769e9")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+}
