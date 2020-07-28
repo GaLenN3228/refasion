70df902493515847d573bad57bd9465b3498c5c2
Kirill Novik <kirill.novik.mail@gmail.com>
2020-07-28 23:52:30 +0300

    cart api implementation

---
+++ lib/screens/product/components/recommended.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:provider/provider.dart';
+import 'package:refashioned_app/models/product.dart';
+import 'package:refashioned_app/models/products.dart';
+import 'package:refashioned_app/repositories/products.dart';
+import 'package:refashioned_app/screens/products/components/products_item.dart';
+
+class RecommendedProducts extends StatelessWidget {
+  final Product product;
+
+  const RecommendedProducts({Key key, this.product})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final ProductsRepository productsRepository =
+    context.watch<ProductsRepository>();
+    if (productsRepository.isLoading)
+      return Center(
+        child: Text("Загрузка", style: Theme.of(context).textTheme.bodyText1),
+      );
+
+    if (productsRepository.loadingFailed)
+      return Center(
+        child: Text("Ошибка", style: Theme.of(context).textTheme.bodyText1),
+      );
+
+    if (productsRepository.productsResponse.status.code != 200)
+      return Center(
+        child: Text("Статус", style: Theme.of(context).textTheme.bodyText1),
+      );
+
+    final ProductsContent productsContent =
+        productsRepository.productsResponse.productsContent;
+
+    return StaggeredGridView.countBuilder(
+      physics: const NeverScrollableScrollPhysics(),
+      shrinkWrap: true,
+      padding: const EdgeInsets.only(top: 16),
+      crossAxisCount: 2,
+      itemCount: productsContent.products.length,
+      itemBuilder: (BuildContext context, int index) => ProductsItem(
+          product: productsContent.products[index]),
+      staggeredTileBuilder: (int index) => new StaggeredTile.fit(1),
+      mainAxisSpacing: 16.0,
+    );
+  }
+}
--- lib/screens/product/content/product.dart
+++ lib/screens/product/content/product.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:refashioned_app/models/product.dart';
 import 'package:refashioned_app/repositories/product.dart';
+import 'package:refashioned_app/repositories/products.dart';
 import 'package:refashioned_app/screens/product/components/add_to_cart.dart';
 import 'package:refashioned_app/screens/product/components/additional.dart';
 import 'package:refashioned_app/screens/product/components/delivery.dart';
@@ -9,14 +10,28 @@ import 'package:refashioned_app/screens/product/components/description.dart';
 import 'package:refashioned_app/screens/product/components/payment.dart';
 import 'package:refashioned_app/screens/product/components/price.dart';
 import 'package:refashioned_app/screens/product/components/questions.dart';
+import 'package:refashioned_app/screens/product/components/recommended.dart';
 import 'package:refashioned_app/screens/product/components/related_products.dart';
 import 'package:refashioned_app/screens/product/components/seller.dart';
 import 'package:refashioned_app/screens/product/components/slider.dart';
 import 'package:refashioned_app/screens/product/components/title.dart';
 
-class ProductPageContent extends StatelessWidget {
+class ProductPageContent extends StatefulWidget {
+  @override
+  _ProductPageContentState createState() => _ProductPageContentState();
+}
+
+class _ProductPageContentState extends State<ProductPageContent> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
+    TextTheme textTheme = Theme
+        .of(context)
+        .textTheme;
     final ProductRepository productRepository = context.watch<ProductRepository>();
     if (productRepository.isLoading)
       return Center(
@@ -64,6 +79,23 @@ class ProductPageContent extends StatelessWidget {
               ProductPayment(),
               ProductAdditional(),
               RelatedProducts(),
+              Column(
+                children: [
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      "ВАМ МОЖЕТ ПОНРАВИТЬСЯ",
+                      style: textTheme.headline2,
+                    ),
+                  ),
+                  ChangeNotifierProvider<ProductsRepository>(
+                      create: (_) => ProductsRepository(parameters: "?p=93d38e10-fa55-417f-9fe0-493e55de5a17"),
+                      builder: (context, _) {
+                        return RecommendedProducts();
+                      }
+                  )
+                ],
+              ),
             ],
           ),
         ),
