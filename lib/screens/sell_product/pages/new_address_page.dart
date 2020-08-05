0c2a42a98786cf5e3a8ceb8a4c43b8d51db4d6c1
Valerii Babanin <valeriy.babanin@remail.pro>
2020-08-05 18:55:23 +0300

    newaddress maps stack fixed

--- lib/screens/sell_product/pages/new_address_page.dart
+++ lib/screens/sell_product/pages/new_address_page.dart
@@ -11,82 +11,90 @@ class NewAddressPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     TextTheme textTheme = Theme.of(context).textTheme;
-    return Container(
-      color: Colors.white,
-      child: Column(
-        children: [
-          SellProductTopBar(TopBarType.newAddress),
-          SizedBox(
-            height: 8,
-          ),
-          Expanded(
-              child: Stack(children: [
-            Expanded(
-              child: Container(
-                child: MapsPage(),
-              ),
+    return CupertinoPageScaffold(
+      child: Container(
+        child: Column(
+          children: [
+            SellProductTopBar(TopBarType.newAddress),
+            SizedBox(
+              height: 8,
             ),
-            Align(
-              alignment: Alignment.bottomCenter,
-              child: ClipRRect(
-                borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(10),
-                    topRight: Radius.circular(10)),
-                child: Container(
-                  color: Colors.white,
-                  height: 160,
-                  child: Column(children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        SizedBox(
-                          height: 13,
+            Expanded(
+              child: Stack(
+                children: [
+                  Container(
+                    child: MapsPage(),
+                  ),
+                  Align(
+                    alignment: Alignment.bottomCenter,
+                    child: Container(
+                      decoration: ShapeDecoration(
+                        color: Colors.white,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(10),
+                            topRight: Radius.circular(10),
+                          ),
                         ),
-                        Container(
-                          width: 30,
-                          height: 3,
-                          decoration: ShapeDecoration(
-                              color: Colors.black.withOpacity(0.1),
-                              shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(100))),
-                        ),
-                      ],
-                    ),
-                    Container(
-                      alignment: Alignment.topLeft,
-                      margin: const EdgeInsets.only(top: 14.0, left: 20),
-                      child: Text(
-                        "Откуда забрать вашу вещь?",
-                        style: textTheme.subtitle1,
-                        maxLines: 1,
-                        overflow: TextOverflow.ellipsis,
+                      ),
+                      height: 160,
+                      child: Column(
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              SizedBox(
+                                height: 13,
+                              ),
+                              Container(
+                                width: 30,
+                                height: 3,
+                                decoration: ShapeDecoration(
+                                    color: Colors.black.withOpacity(0.1),
+                                    shape: RoundedRectangleBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(100))),
+                              ),
+                            ],
+                          ),
+                          Container(
+                            alignment: Alignment.topLeft,
+                            margin: const EdgeInsets.only(top: 14.0, left: 20),
+                            child: Text(
+                              "Откуда забрать вашу вещь?",
+                              style: textTheme.subtitle1,
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ),
+                          Container(
+                            alignment: Alignment.topLeft,
+                            margin: const EdgeInsets.only(top: 10.0, left: 20),
+                            child: Text(
+                              "Укажите на карте или введите адрес вручную",
+                              style: textTheme.caption,
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(20),
+                            child: Button(
+                              "ВВЕСТИ АДРЕС",
+                              buttonStyle: ButtonStyle.amber,
+                              height: 45,
+                              width: double.infinity,
+                              borderRadius: 5,
+                              onClick: onPush,
+                            ),
+                          )
+                        ],
                       ),
                     ),
-                    Container(
-                      alignment: Alignment.topLeft,
-                      margin: const EdgeInsets.only(top: 10.0, left: 20),
-                      child: Text(
-                        "Укажите на карте или введите адрес вручную",
-                        style: textTheme.caption,
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(20),
-                      child: Button(
-                        "ВВЕСТИ АДРЕС",
-                        buttonStyle: ButtonStyle.amber,
-                        height: 45,
-                        width: double.infinity,
-                        borderRadius: 5,
-                        onClick: onPush,
-                      ),
-                    )
-                  ]),
-                ),
+                  )
+                ],
               ),
             )
-          ]))
-        ],
+          ],
+        ),
       ),
     );
   }
