917d6b631b7655185472a6dacc017cfb8a88d8a4
Kirill Novik <kirill.novik.mail@gmail.com>
2020-08-31 17:41:40 +0300

    maps changes https://refashioned.atlassian.net/browse/MOB-11

--- lib/screens/maps/components/map.dart
+++ lib/screens/maps/components/map.dart
@@ -40,7 +40,6 @@ class _MapPageState extends State<MapPage> {
 
   @override
   void initState() {
-
     if (widget.mapDataController.pickUpPointsCompany != null) {
       pickPointRepository = new PickPointRepository();
       pickPointRepository.addListener(() {
@@ -60,7 +59,7 @@ class _MapPageState extends State<MapPage> {
   Future<void> _requestPermission() async {
     final List<PermissionGroup> permissions = <PermissionGroup>[PermissionGroup.location];
     final Map<PermissionGroup, PermissionStatus> permissionRequestResult =
-        await PermissionHandler().requestPermissions(permissions);
+    await PermissionHandler().requestPermissions(permissions);
     setState(() {
       _permissionStatus = permissionRequestResult[PermissionGroup.location];
     });
@@ -119,7 +118,7 @@ class _MapPageState extends State<MapPage> {
     if (_permissionStatus == PermissionStatus.granted) {
       _allowMapTouch = false;
       await controller.move(
-          zoom: 10,
+          zoom: 15,
           point: Point(latitude: latitude, longitude: longitude),
           animation: const MapAnimation(smooth: true, duration: 2.0));
     }
@@ -133,20 +132,21 @@ class _MapPageState extends State<MapPage> {
           point: _point,
           opacity: 0.8,
           iconName: 'assets/marker_red_small.png',
-          onTap: (Placemark placemark, double latitude, double longitude) => {
-            if (selectedPlaceMark != null)
-              {
-                controller.changePlacemarkIcon(
-                    selectedPlaceMark,
-                    markerType == MarkerType.POINT_MEDIUM
-                        ? 'assets/marker_red_medium.png'
-                        : 'assets/marker_red_small.png')
-              },
-            selectedPlaceMark = placemark,
-            controller.changePlacemarkIcon(placemark, 'assets/marker_red_selected.png'),
-            widget.onMarkerClick(pickPointRepository.response.content.firstWhere((element) =>
-                double.parse(element.lat) == selectedPlaceMark.point.latitude &&
-                double.parse(element.lon) == selectedPlaceMark.point.longitude))
+          onTap: (Placemark placemark, double latitude, double longitude) {
+            if (selectedPlaceMark != null) {
+              controller.changePlacemarkIcon(
+                  selectedPlaceMark,
+                  markerType == MarkerType.POINT_MEDIUM
+                      ? 'assets/marker_red_medium.png'
+                      : 'assets/marker_red_small.png');
+            }
+            selectedPlaceMark = placemark;
+            controller.changePlacemarkIcon(placemark, 'assets/marker_red_selected.png');
+            var point = pickPointRepository.response.content.firstWhere((element) =>
+            double.parse(element.lat) == selectedPlaceMark.point.latitude &&
+                double.parse(element.lon) == selectedPlaceMark.point.longitude);
+            moveToPoint(15, double.parse(point.lat), double.parse(point.lon));
+            widget.onMarkerClick(point);
           },
         );
         placeMarks.add(_placemark);
