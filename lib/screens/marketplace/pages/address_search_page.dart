d2a475ec08169c2a2b7c5ee3ffa6bcf83f40491e
Valerii Babanin <valeriy.babanin@remail.pro>
2020-09-02 19:13:47 +0300

    removed ref scaffold from address_search_page

--- lib/screens/marketplace/pages/address_search_page.dart
+++ lib/screens/marketplace/pages/address_search_page.dart
@@ -4,13 +4,11 @@ import 'package:refashioned_app/models/addresses.dart';
 import 'package:refashioned_app/repositories/addresses.dart';
 import 'package:refashioned_app/screens/catalog/filters/components/sliding_panel_indicator.dart';
 import 'package:refashioned_app/screens/components/items_divider.dart';
-import 'package:refashioned_app/screens/components/scaffold/data/children_data.dart';
-import 'package:refashioned_app/screens/components/scaffold/data/scaffold_data.dart';
-import 'package:refashioned_app/screens/components/scaffold/scaffold.dart';
 import 'package:refashioned_app/screens/components/topbar/data/tb_button_data.dart';
 import 'package:refashioned_app/screens/components/topbar/data/tb_data.dart';
 import 'package:refashioned_app/screens/components/topbar/data/tb_middle_data.dart';
 import 'package:refashioned_app/screens/components/topbar/data/tb_search_data.dart';
+import 'package:refashioned_app/screens/components/topbar/top_bar.dart';
 import 'package:refashioned_app/screens/marketplace/components/address_tile.dart';
 
 class AddressSearchPage extends StatefulWidget {
@@ -43,68 +41,67 @@ class _AddressSearchPageState extends State<AddressSearchPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        SlidingPanelIndicator(),
-        Expanded(
-          child: RefashionedScaffold(
-            data: ScaffoldData(
-              coveredWithBottomNav: false,
-              scrollController: widget.scrollController,
-              topBarData: TopBarData(
-                middleData: TBMiddleData.title("Поиск"),
-                searchData: TBSearchData(
-                  hintText: "Введите адрес",
-                  onSearchUpdate: (newQuery) =>
-                      addressesRepository.findAddressesByQuery(newQuery),
-                  autofocus: true,
-                ),
-                rightButtonData: TBButtonData.text("Закрыть",
-                    onTap: Navigator.of(context).pop),
+    return CupertinoPageScaffold(
+      backgroundColor: Colors.white,
+      child: Column(
+        children: [
+          SlidingPanelIndicator(),
+          RefashionedTopBar(
+            data: TopBarData(
+              middleData: TBMiddleData.title("Поиск"),
+              searchData: TBSearchData(
+                hintText: "Введите адрес",
+                onSearchUpdate: (newQuery) =>
+                    addressesRepository.findAddressesByQuery(newQuery),
+                autofocus: true,
               ),
-              childrenDataStream:
-                  addressesRepository.addressesProvider.addresses.map(
-                (addresses) {
-                  if (addresses.isEmpty)
-                    return ScaffoldChildrenData.single(
-                      Container(
-                        padding: const EdgeInsets.all(20),
-                        alignment: Alignment.topLeft,
-                        child: Text(
-                          "Вы можете искать по любым объектам на карте, включая города, улицы, метро и достопримечательности",
-                          style: Theme.of(context).textTheme.subtitle2,
-                        ),
-                      ),
-                    );
-
-                  return ScaffoldChildrenData.separated(
-                    addresses.length,
-                    (context, index) => AddressTile(
-                      address: addresses.elementAt(index),
-                      onTap: () {
-                        widget.onSelect(addresses.elementAt(index));
-                        Navigator.of(context).pop();
-                      },
-                    ),
-                    (context, index) => ItemsDivider(),
-                  );
-                },
-              ),
-              onStreamError: (error) => ScaffoldChildrenData.single(
-                Container(
-                  padding: const EdgeInsets.all(20),
-                  alignment: Alignment.topLeft,
-                  child: Text(
-                    "Ничего не найдено",
-                    style: Theme.of(context).textTheme.subtitle2,
-                  ),
-                ),
-              ),
+              rightButtonData: TBButtonData.text("Закрыть",
+                  onTap: Navigator.of(context).pop),
             ),
           ),
-        ),
-      ],
+          Expanded(
+            child: StreamBuilder<List<Address>>(
+              stream: addressesRepository.addressesProvider.addresses,
+              builder: (context, snapshot) {
+                if (snapshot.hasError)
+                  return Container(
+                    padding: const EdgeInsets.all(20),
+                    alignment: Alignment.topLeft,
+                    child: Text(
+                      "Ничего не найдено",
+                      style: Theme.of(context).textTheme.subtitle2,
+                    ),
+                  );
+
+                final addresses = snapshot.data;
+
+                if (!snapshot.hasData || addresses.isEmpty)
+                  return Container(
+                    padding: const EdgeInsets.all(20),
+                    alignment: Alignment.topLeft,
+                    child: Text(
+                      "Вы можете искать по любым объектам на карте, включая города, улицы, метро и достопримечательности",
+                      style: Theme.of(context).textTheme.subtitle2,
+                    ),
+                  );
+
+                return ListView.separated(
+                  controller: widget.scrollController,
+                  itemCount: addresses.length,
+                  itemBuilder: (context, index) => AddressTile(
+                    address: addresses.elementAt(index),
+                    onTap: () {
+                      widget.onSelect(addresses.elementAt(index));
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                  separatorBuilder: (context, index) => ItemsDivider(),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
