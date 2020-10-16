46dfd1c30e653b03105cd64743a42f753ef2b989
Valerii Babanin <valeriy.babanin@remail.pro>
2020-10-16 17:25:14 +0300

    generated splash screens for ios and android

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -9,6 +9,9 @@ import YandexMapKit
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     YMKMapKit.setApiKey("721bea2e-5bfb-48d2-9a3f-f5e6311769e9")
+    var flutter_native_splash = 1
+    UIApplication.shared.isStatusBarHidden = false
+
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
